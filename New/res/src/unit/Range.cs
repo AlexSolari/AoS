da7653be074db2f517d169c93f35b2e94e9cd29d
vuniverTeam <gearbots@yandex.com>
2015-04-07 17:19:23 +0300

    sprites

---
+++ New/res/src/unit/Range.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+
+namespace New.res.src.unit
+{
+    class Range : Warrior
+    {
+        public Range(int team, Point position)
+            : base(@"range.png", team, position, Type.range)
+        {
+
+        }
+    }
+}
---
+++ New/res/src/unit/Siege.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otter;
+
+namespace New.res.src.unit
+{
+    class Siege : Warrior
+    {
+        public Siege(int team, Point position)
+            : base(@"siege.png", team, position, Type.siege)
+        {
+
+        }
+    }
+}
